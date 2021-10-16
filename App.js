32ee4cc312b83db1e8b1d00eb6888a9b97935f65
joao07 <92588075+joao07@users.noreply.github.com>
2021-10-16 19:30:28 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,10 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Primeiro from './Components/Primeiro';
+import Segundo from './Components/Segundo';
+import {comp1, comp2} from './Components/Comp'
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!!</Text>
+      <Primeiro/>
+      <Segundo/>
+      <comp1/>
+      <comp2/>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +27,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const style = StyleSheet.create({
+  App: {
+      backgroundColor: "#A00",
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+
+
+  }
+})
