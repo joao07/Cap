ef945f3a46601bae1954baa04244cb76a9c136db
joao07 <92588075+joao07@users.noreply.github.com>
2021-10-17 22:36:54 -0300

    Create FlexBoxV3.js

---
+++ layout/FlexBoxV3.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import TxtG from "../Styles/TxtG";
+import Quadrado from "./Quadrado";
+
+export default props => {
+    return (
+        <View style = {styleTest.FlexV2}>
+            <Quadrado cor='#fff'/>
+            <Quadrado cor='#fcba03'/>
+            <Quadrado cor='#0048ff'/>
+            <Quadrado cor='#263866'/>
+            <Quadrado cor='#5b1061'/>
+        </View>
+    )
+}
+
+const styleTest = StyleSheet.create({
+    FlexV2:{
+        flex: 1,
+        backgroundColor: '#000',
+        alignItems:'center'
+    }
+})
