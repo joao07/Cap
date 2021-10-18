5b0949dcddfa61c288930c9c71fd211eeaef23cf
joao07 <92588075+joao07@users.noreply.github.com>
2021-10-17 22:36:35 -0300

    Update Segundo.js

---
+++ Components/Segundo.js
@@ -0,0 +1,13 @@
+import { Text, StyleSheet } from "react-native";
+
+export default function Senha() {
+   return(
+   <Text>Senha</Text>
+   );
+}
+
+const StyleSenha = StyleSheet.create({
+    Estilo:{
+        textDecorationLine: 'underline'
+    }
+});
