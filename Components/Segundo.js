98449796d46cff13582b972605b189f544cc100c
joao07 <92588075+joao07@users.noreply.github.com>
2021-10-19 22:28:18 -0300

    Update Segundo.js

--- Components/Segundo.js
+++ Components/Segundo.js
@@ -1,13 +1,17 @@
+import React from "react";
 import { Text, StyleSheet } from "react-native";
 
 export default function Senha() {
-   return(
-   <Text>Senha</Text>
-   );
+   return<Text style = {StyleSenha.Estilo}>Senha</Text>
+   
 }
 
 const StyleSenha = StyleSheet.create({
     Estilo:{
-        textDecorationLine: 'underline'
+        textDecorationLine: 'underline',
+        width: '80%',
+        alignItems:'center',
+        color:'#636963',        
+        fontSize: 20,
     }
 });
