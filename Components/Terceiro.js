0a721734543c2df8c21517c7fe976bfb3f09aa53
joao07 <92588075+joao07@users.noreply.github.com>
2021-10-19 22:28:20 -0300

    Update Terceiro.js

---
+++ Components/Terceiro.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+export default function ResetPassword() {
+   return<Text style = {StyleReset.Estilo}>Refaça a Senha</Text>
+   
+}
+
+const StyleReset = StyleSheet.create({
+    Estilo:{
+        alignItems: 'center'
+    }
+});
