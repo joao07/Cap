24d095c4a72eb877224050ffb0142f7242d5616a
joao07 <92588075+joao07@users.noreply.github.com>
2021-10-20 15:39:19 -0300

    Update Primeiro.js

--- Components/Primeiro.js
+++ Components/Primeiro.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, TextInput } from "react-native";
 
-export default function Email() {
-   return<Text style = {StyleEmail.Estilo}>Email</Text>
-   
-}
+export default function EmailInput () {
+    const [text, onChangeText] = React.useState(null);
+  
+    return (
+        <TextInput
+        style={StyleEmail.input}
+          onChangeText={onChangeText}
+          value={text}
+          keyboardType='email-address'
+          placeholder="Email"
+        />
+    );
+  };
 
 const StyleEmail = StyleSheet.create({
     Estilo:{
@@ -14,5 +23,18 @@ const StyleEmail = StyleSheet.create({
         fontSize: 20,
         color:'#636963',
         
-    }
+    },
+
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        marginLeft:10,
+        backgroundColor:'whitesmoke',
+        marginTop: 10,
+        justifyContent:'center',
+        borderRadius: 20
+      },
 });
+
