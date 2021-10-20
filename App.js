d0cfe75a4a325dab2b3089df71e74fc15c121f5a
joao07 <92588075+joao07@users.noreply.github.com>
2021-10-20 15:39:09 -0300

    Update App.js

--- App.js
+++ App.js
@@ -7,50 +7,15 @@ import TelaLogin from './Telas/Login';
 
 export default function App() {
   return (
-      <View>
+      <SafeAreaView>
         <TelaLogin/>
-      </View>
+      </SafeAreaView>
   );
 }
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  EstiloLogin:{
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 20,
-  },
-
-  EstiloCaixa:{
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '70%',
-    height: '10%',
-    backgroundColor: '#266621',
-    marginLeft: 50,
-    marginTop: 20,
-},
-
-EstiloText:{
-    marginLeft:10,
-    width:'90%',
-    backgroundColor:'#c5d1c5',
-    marginTop: 10,
-    height:'10%',
-    justifyContent:'center'
-},
-
 App: {
-  backgroundColor: "#A00",
-  flex: 1,
-  justifyContent: "center",
-  alignItems: "center"
+  backgroundColor: "#3f3f40",
 }
 });
