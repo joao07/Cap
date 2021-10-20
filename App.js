bb40d71c5a664786968e84bd391832cfa42988c3
joao07 <92588075+joao07@users.noreply.github.com>
2021-10-19 22:27:59 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,28 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
-import Primeiro from './Components/Primeiro';
-import Segundo from './Components/Segundo';
-import {comp1, comp2} from './Components/Comp';
-import FlexBoxV1 from './layout/FlexBoxV1';
-import Terceiro from './Components/Terceiro';
-import Quarto from './Components/Quarto';
-import Quinto from './Components/Quinto';
+import TelaLogin from './Telas/Login';
 
-export default function App() {
-  return (
-    <SafeAreaView>
-      <FlexBoxV1/>
-      <Primeiro/>
-      <Segundo/>
-      <Terceiro/>
-      <Quarto/>
-      <Quinto/>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
-}
+<TelaLogin/>
 
 const styles = StyleSheet.create({
   container: {
@@ -31,15 +13,36 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  EstiloLogin:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+
+  EstiloCaixa:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '70%',
+    height: '10%',
+    backgroundColor: '#266621',
+    marginLeft: 50,
+    marginTop: 20,
+},
+
+EstiloText:{
+    marginLeft:10,
+    width:'90%',
+    backgroundColor:'#c5d1c5',
+    marginTop: 10,
+    height:'10%',
+    justifyContent:'center'
+},
+
+App: {
+  backgroundColor: "#A00",
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center"
+}
 });
-
-const style = StyleSheet.create({
-  App: {
-      backgroundColor: "#A00",
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center"
-
-
-  }
-})
