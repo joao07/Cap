11b07d040702205db6eac74698f9100637f445c4
joao07 <92588075+joao07@users.noreply.github.com>
2021-10-19 22:28:40 -0300

    Update FlexBoxV4.js

--- layout/FlexBoxV4.js
+++ layout/FlexBoxV4.js
@@ -5,20 +5,26 @@ import Quadrado from "./Quadrado";
 
 export default props => {
     return (
-        <View style = {styleTest.FlexV2}>
-            <Quadrado cor='#fff'/>
-            <Quadrado cor='#fcba03'/>
-            <Quadrado cor='#0048ff'/>
-            <Quadrado cor='#263866'/>
-            <Quadrado cor='#5b1061'/>
+        <View style = {styleTest.FlexV4}>
+            <View style = {styleTest.V1}/>
+            <View style = {styleTest.V2}/>
         </View>
     )
 }
 
 const styleTest = StyleSheet.create({
-    FlexV2:{
-        flex: 1,
+    FlexV4:{
+        flexGrow: 1,
+        width: 100,
         backgroundColor: '#000',
         alignItems:'center'
+    },
+
+    V1:{
+
+    },
+
+    V2:{
+
     }
 })
